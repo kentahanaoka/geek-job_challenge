c2e57f249b60671d01047aef31ead1edbd2910da
DESKTOP-1RM00CR\guest1Day <ggrkslololo@gmail.com>
2018-07-03 17:31:44 +0900

    サーブレットとｊｓｐの課題を提出させていただきます

--- CampChallenge_java/CampChallenge_java/009.サーブレット/SampleA/src/java/org/camp/servlet/FourtuneTelling.java
+++ CampChallenge_java/CampChallenge_java/009.サーブレット/SampleA/src/java/org/camp/servlet/FourtuneTelling.java
@@ -8,24 +8,20 @@ package org.camp.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Random;
 import java.util.Date;
 import java.util.Random;
 import javax.servlet.RequestDispatcher;
 import org.camp.servlet.ResultData;
-
-
-
-
-
 /**
  *
  * @author guest1Day
  */
-public class FortuneTelling extends HttpServlet {
+@WebServlet(name = "FourtuneTelling", urlPatterns = {"/FourtuneTelling"})
+public class FourtuneTelling extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -41,21 +37,27 @@ public class FortuneTelling extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-            String[] luckList = {"大吉", "中吉", "吉", "半吉", "末小吉", "凶", "小凶", "半凶", "末凶", "凶", "大凶"};
+            
+           final String result = "/WEB-INF/jsp/FortuneTellingResult.jsp";
+            String luckList[] = {"大吉", "中吉", "吉", "半吉", "末小吉", "凶", "小凶", "半凶", "末凶", "凶", "大凶"};
+            
             Random rand = new Random();
-            Integer index = rand.nextInt(luckList.length);
-            
-            out.print("今日の運勢は。。。");
-            out.print(luckList[index]);
-            
-            ResultData data = new ResultData();
-            data.setD(new Data());
-            data.setLuck(luckList[index]);
-            request.setAttribute("DATA",data);
 
-            RequestDispatcher rd = request.getRequestDispatcher(result);
-            rd.forward(request,response);
+Integer index = rand.nextInt(luckList.length);
+         out.println("あなたの今日の運勢は・・・");
+         out.print(luckList[index]);
+         
+         ResultData data = new ResultData();
+         data.setD(new Date());
+         data.setLuck(luckList[index]);
+         request.setAttribute("DATA",data);
 
+         RequestDispatcher rd =request.getRequestDispatcher(result);
+         rd.forward(request, response);
+
+         
+         
+         
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
@@ -68,6 +70,8 @@ public class FortuneTelling extends HttpServlet {
         }
     }
 
+
+    
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
---
+++ CampChallenge_java/CampChallenge_java/009.サーブレット/org.camp.servlet/web/WEB-INF/jsp/FourtuneTellingResult.jsp
@@ -0,0 +1,26 @@
+<%-- 
+    Document   : FourtuneTellingResult
+    Created on : 2018/07/02, 14:16:05
+    Author     : guest1Day
+--%>
+
+
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+     <%
+     package org.camp.servlet;
+     
+
+     
+     
+     
+     %>
+    </body>
+</html>
