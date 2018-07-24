2e9525082a9fa82d069506953b2b418f11b57fc3
DESKTOP-1RM00CR\guest1Day <ggrkslololo@gmail.com>
2018-07-24 16:26:35 +0900

    クラス課題１提出させていただきます

--- CampChallenge_java/CampChallenge_java/013.クラス/class1/Main1.java
+++ CampChallenge_java/CampChallenge_java/013.クラス/class1/Main1.java
@@ -1,3 +1,7 @@
+package class1;
+
+
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,16 +12,15 @@
  *
  * @author guest1Day
  */
-public class Main2 {
+public class Main1 {
     public static void main(String[] args){
-    Class2 hito=new Class2();
-   
-    //setProfile呼び出し
-    hito.setProfile("健太花岡",25,"所沢");
-    hito.printProfile();
-    //データをクリア
-    hito.clearProfile();
-    hito.printProfile();
-    
+Class1 hito=new Class1();
+hito.name="花岡健太";
+hito.age=25;
+hito.address="所沢";
+
+//hito.setProfile("花岡健太",25,"所沢");
+  
+hito.printProfile();
     }
 }
