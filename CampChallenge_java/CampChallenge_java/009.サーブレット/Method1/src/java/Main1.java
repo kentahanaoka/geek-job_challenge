f60db9ecd1d16c945f1dd20501b6be98fa753e01
DESKTOP-1RM00CR\guest1Day <ggrkslololo@gmail.com>
2018-07-24 14:18:20 +0900

    クラス課題提出させていただきます

---
+++ CampChallenge_java/CampChallenge_java/009.サーブレット/Method1/src/java/Main1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author guest1Day
+ */
+public class Main1 {
+    public static void main(String[] args){
+Class1 hito=new Class1();
+hito.name="花岡健太";
+hito.age=25;
+hito.address="所沢";
+
+hito.setProfile("花岡健太",25,"所沢");
+  
+hito.printProfile();
+    }
+}
