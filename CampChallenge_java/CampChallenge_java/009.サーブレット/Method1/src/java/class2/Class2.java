b5cdeaa0b860bf26528d745fe2db165f7e24ecf1
DESKTOP-1RM00CR\guest1Day <ggrkslololo@gmail.com>
2018-07-24 16:41:05 +0900

    クラス２課題提出させていただきます

---
+++ CampChallenge_java/CampChallenge_java/009.サーブレット/Method1/src/java/class2/Class2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author guest1Day
+ */
+
+
+//クラス継承
+public class Class2 extends Class1 {
+ 
+ public void clearProfile(){
+//数値のクリア
+name=null;
+age=0;
+address=null;
+}
+ 
+}
---
+++ CampChallenge_java/CampChallenge_java/009.サーブレット/Method1/src/java/class2/Main2.java
@@ -0,0 +1,27 @@
+package class2;
+
+import .*;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author guest1Day
+ */
+public class Main2 {
+    public static void main(String[] args){
+    Class2 hito=new Class2();
+   
+    //setProfile呼び出し
+    hito.setProfile("健太花岡",25,"所沢");
+    hito.printProfile();
+    //データをクリア
+    hito.clearProfile();
+    hito.printProfile();
+    
+    }
+}
