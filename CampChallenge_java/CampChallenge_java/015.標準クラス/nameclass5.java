a174fb5169de2f791ed31b0e356961f408afb22c
DESKTOP-1RM00CR\guest1Day <ggrkslololo@gmail.com>
2018-08-07 13:40:18 +0900

    標準クラス課題５提出させていただきます

---
+++ CampChallenge_java/CampChallenge_java/015.標準クラス/nameclass5.java
@@ -0,0 +1,22 @@
+import java.io.PrintStream;
+import java.util.Calendar;
+import java.util.Date;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author guest1Day
+ */
+public class nameclass5 {
+     public static void main(String[] args) {
+   String name="花岡健太";
+   System.out.println(name.getBytes().length); 
+   System.out.print(name.length());
+    
+}
+}
