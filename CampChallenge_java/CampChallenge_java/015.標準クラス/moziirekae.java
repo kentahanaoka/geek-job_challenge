84c6e6ebf358cb5ac68031838f5b6e4f42917b5b
DESKTOP-1RM00CR\guest1Day <ggrkslololo@gmail.com>
2018-08-07 14:38:27 +0900

    標準クラス文字の置換課題を提出させていただきます。

---
+++ CampChallenge_java/CampChallenge_java/015.標準クラス/moziirekae.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author guest1Day
+ */
+public class moziirekae {
+     public static void main(String[] args){
+         String a=("きょUはぴIえIちぴIのくみこみかんすUのがくしゅUをしてIます");
+         a=a.replace("I", "い");
+         a=a.replace("U","う");
+        
+         System.out.print(a);
+         //System.out.print(a.replace("U","う"));
+         
+     
+     
+     }
+     
+}
